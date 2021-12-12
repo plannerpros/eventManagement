cd50a75b70c29ebc65a8d56b8262c822e1209fca
ChethanLReddy <chethanlreddy@icloud.com>
2021-12-12 19:01:12 +0530

    feedback 12

--- app/src/main/java/com/example/planner/feedback1.java
+++ app/src/main/java/com/example/planner/feedback1.java
@@ -14,27 +14,6 @@ public class feedback1 extends AppCompatActivity {
         setContentView(R.layout.activity_feedback);
 
 
-        EditText nameEditText = (EditText) findViewById(R.id.names);
-        String fullName = nameEditText.getText().toString();
-    }
 
-
-    public void radioButtonhandler(View view) {
-
-        // Decide what happens when a user clicks on a button
-    }
-
-
-    EditText ageEditText = (EditText) findViewById(R.id.age);
-    String age = ageEditText.getText().toString();
-
-    EditText phoneEditText = (EditText) findViewById(R.id.names);
-    String phone = phoneEditText.getText().toString();
-
-
-    public void submitbuttonHandler(View view) {
-        //Decide what happens when the user clicks the submit button
-
-    }
-
+}
 }
