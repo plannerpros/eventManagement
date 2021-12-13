3212e4d6cbf54f4a081f62769c4dc851e7a626a2
gau2001 <gau04gau@gmail.com>
2021-12-13 10:19:05 +0530

    feedback form

--- app/src/main/java/com/example/planner/feedback1.java
+++ app/src/main/java/com/example/planner/feedback1.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class feedback1 extends AppCompatActivity {
@@ -12,8 +13,33 @@ public class feedback1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
+        EditText nameEditText = (EditText) findViewById(R.id.names);
+        String fullName = nameEditText.getText().toString();
 
+        EditText ageEditText = (EditText) findViewById(R.id.age);
+        String age = ageEditText.getText().toString();
 
+        EditText phoneEditText = (EditText) findViewById(R.id.names);
+        String phone = phoneEditText.getText().toString();
 
+        EditText addressEditText = (EditText) findViewById(R.id.names);
+        String address = addressEditText.getText().toString();
+
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+            }
+        });
+    }
+    public void radioButtonhandler(View view) {
+
+        // Decide what happens when a user clicks on a button
+    }
+    public void submitbuttonHandler(View view) {
+        //Decide what happens when the user clicks the submit button
+
+    }
 }
-}
+
+
