875fc22816de1d14d1ea6a8bb0074403ff6d0e49
ChethanLReddy <chethanlreddy@icloud.com>
2021-12-15 16:11:51 +0530

    fixed feedback and added date and time to the fedd back

--- app/src/main/java/com/example/planner/feedback1.java
+++ app/src/main/java/com/example/planner/feedback1.java
@@ -8,26 +8,41 @@ import com.example.planner.activity_collectdetails;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
+import java.util.*;
 
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class feedback1 extends AppCompatActivity {
+    public static final String TAG = "Tag";
     RadioButton rb1, rb2, rb3, rb4, rb5, rb6;
     Button b1;
-    FirebaseDatabase fd;
-    DatabaseReference ar;
-    DatabaseReference ref;
+//    FirebaseDatabase fd;
+//    DatabaseReference ar;
+//    DatabaseReference ref;
     EditText e1,e2;
+    FirebaseAuth auth;
+    FirebaseFirestore fbs;
+    String userId;
 
 
     @Override
@@ -45,15 +60,17 @@ public class feedback1 extends AppCompatActivity {
         rb4 = findViewById(R.id.depends);
         rb5 = findViewById(R.id.later);
         rb6 = findViewById(R.id.likely);
-
         b1 = findViewById(R.id.submit_button);
+
+        fbs = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+
         final String[] satisfied = new String[1];
         final String[] futureEvents = new String[1];
-        fd = FirebaseDatabase.getInstance();
-        ar = fd.getReference().child("feedback");
+        //fd = FirebaseDatabase.getInstance();
+        //ar = fd.getReference().child("feedback");
 
-        String suggestions = e2.getText().toString();
-        String takeAway = e1.getText().toString();
+
 
         /*ref = fd.getReference().child("users.phNumber");
         ref.addValueEventListener(new ValueEventListener() {
@@ -88,6 +105,10 @@ public class feedback1 extends AppCompatActivity {
                     futureEvents[0] = rb6.getText().toString();
                 }
 
+                String suggestions = e2.getText().toString();
+                String takeAway = e1.getText().toString();
+
+
                 String sat = satisfied[0];
                 String future = futureEvents[0];
                 String sug = suggestions;
@@ -95,8 +116,30 @@ public class feedback1 extends AppCompatActivity {
                 //ar.child("Satisfaction").setValue(satisfied[0]);
                 //ar.child("future event").setValue(futureEvents[0]);
                 //System.out.println("hey!" + satisfied[0].toString() + " " + futureEvents[0]+toString());
-                feedbackDatabase fb = new feedbackDatabase(sat,future,sug,take);
-                ar.child("7820404416").setValue(fb);
+                //feedbackDatabase fb = new feedbackDatabase(sat,future,sug,take);
+                //ar.child("7820404416").setValue(fb);
+                userId = auth.getCurrentUser().getUid();
+                DocumentReference docuRefr = fbs.collection("Feedback").document(userId);
+                Map<String,Object> user = new HashMap<>();
+                user.put("satisfaction",sat);
+                user.put("Future Booking",future);
+                user.put("Suggestions",sug);
+                user.put("Take away ",take);
+                long currentTime=System.currentTimeMillis();
+                Calendar cal=Calendar.getInstance();
+                cal.setTimeInMillis(currentTime);
+                System.out.println(currentTime+"time and date"+cal);
+                user.put("Time:",currentTime);
+                user.put("Date:",cal);
+                docuRefr.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(@NonNull Void unused) {
+                        Log.d(TAG,"onSucces: user feedback is sucesfull "+userId);
+
+                        Toast.makeText(feedback1.this, "You are Done", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
 
                 Intent i = new Intent(feedback1.this,dashboard.class);
                 startActivity(i);
