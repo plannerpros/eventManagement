0735b0eb839506c7b305ad89c095ffee94ba44f7
ChethanLReddy <chethanlreddy@icloud.com>
2022-01-06 13:29:12 +0530

    venue chooser

--- app/src/main/java/com/example/planner/activity_venueChooser.java
+++ app/src/main/java/com/example/planner/activity_venueChooser.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class activity_venueChooser extends AppCompatActivity {
+    FirebaseDatabase fireData;
+    DatabaseReference dataRefrence;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_venue_chooser);
+        fireData = FirebaseDatabase.getInstance();
+        dataRefrence = FirebaseDatabase.getInstance().getReference("venues");
+
+
     }
 }
