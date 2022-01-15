045085aa59579642f9311e99c0cd4220ac0c1fac
ChethanLReddy <chethanlreddy@icloud.com>
2022-01-15 14:27:48 +0530

    fixed bug

--- app/src/main/java/com/example/planner/activity_eventType.java
+++ app/src/main/java/com/example/planner/activity_eventType.java
@@ -1,11 +1,15 @@
 package com.example.planner;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class activity_eventType extends AppCompatActivity {
-
+ImageButton backButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,6 +17,15 @@ public class activity_eventType extends AppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
+        backButton =  findViewById(R.id.previous_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(activity_eventType.this,planning.class);
+                startActivity(i);
+                //startActivity(new Intent(getApplicationContext(),planning.class));
+            }
+        });
 
     }
 }
