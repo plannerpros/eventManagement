cc69d679d59a71525ab4e281cdeb51d37399afcc
ChethanLReddy <chethanlreddy@icloud.com>
2022-01-15 16:51:11 +0530

    evvent typoe

--- app/src/main/java/com/example/planner/activity_eventType.java
+++ app/src/main/java/com/example/planner/activity_eventType.java
@@ -9,7 +9,9 @@ import android.view.View;
 import android.widget.ImageButton;
 
 public class activity_eventType extends AppCompatActivity {
-ImageButton backButton;
+ImageButton backButton,birthdayButt,marrageButt,nameCerobutton,getTogetherbutt,farewellButt,housewarmingButt,teaPartybutt,customButton;
+ String eventType;
+String eventName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +20,60 @@ ImageButton backButton;
             getSupportActionBar().hide();
         }
         backButton =  findViewById(R.id.previous_button);
+        birthdayButt = findViewById(R.id.birthday__);
+        marrageButt = findViewById(R.id.marriage);
+        nameCerobutton = findViewById(R.id.naming);
+        getTogetherbutt = findViewById(R.id.get_together);
+        farewellButt = findViewById(R.id.farewell);
+        housewarmingButt = findViewById(R.id.house_warming);
+        teaPartybutt = findViewById(R.id.tea_party);
+        //customButton = findViewById(R.id.);
+        birthdayButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                eventType =  "Birthday";
+                System.out.println(eventType);
+            }
+        });
+        marrageButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                eventType="Marrage";
+            }
+        });
+        nameCerobutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                eventType = "Naming Ceremony ";
+            }
+        });
+        getTogetherbutt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                eventType = "Get Toghter";
+            }
+        });
+        farewellButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                eventType = "Farewell";
+            }
+        });
+        housewarmingButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                eventType = "House Warming";
+            }
+        });
+        teaPartybutt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                eventType = "Tea Party";
+            }
+        });
+
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
