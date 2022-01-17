b8fdfef9655ea24b5bbbf575a91d2444df6165f2
Arjun S Pramod <ascorncup@gmail.com>
2022-01-17 12:35:24 +0530

    Venue chooser module set to go!

--- app/src/main/java/com/example/planner/activity_venueChooser.java
+++ app/src/main/java/com/example/planner/activity_venueChooser.java
@@ -1,8 +1,12 @@
 package com.example.planner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -10,6 +14,10 @@ import com.google.firebase.database.FirebaseDatabase;
 public class activity_venueChooser extends AppCompatActivity {
     FirebaseDatabase fireData;
     DatabaseReference dataRefrence;
+    ImageButton previousButton, searchButton;
+    EditText searchBar;
+    RecyclerView recyclerListView;
+
 
 
 
@@ -19,6 +27,10 @@ public class activity_venueChooser extends AppCompatActivity {
         setContentView(R.layout.activity_venue_chooser);
         fireData = FirebaseDatabase.getInstance();
         dataRefrence = FirebaseDatabase.getInstance().getReference("venues");
+        searchBar = findViewById(R.id.search_bar);
+        searchButton = findViewById(R.id.search_button);
+        previousButton = findViewById(R.id.previous_button);
+
 
 
     }
