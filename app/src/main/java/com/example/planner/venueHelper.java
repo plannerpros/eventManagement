d0cdc221416abaed5e807fc3986efbfb52953d96
Arjun S Pramod <ascorncup@gmail.com>
2022-01-17 12:46:29 +0530

    Venue chooser module set to go!

---
+++ app/src/main/java/com/example/planner/venueHelper.java
@@ -0,0 +1,35 @@
+package com.example.planner;
+
+public class venueHelper {
+    String title,image,description;
+
+    public venueHelper(String title, String image, String description) {
+        this.title = title;
+        this.image = image;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
