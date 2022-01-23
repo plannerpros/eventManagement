76e015f6bfd12e177d3b5fd67d528b89ea3ad59e
ChethanLReddy <chethanlreddy@icloud.com>
2022-01-23 14:56:01 +0530

    services working with data base

--- app/src/main/java/com/example/planner/venueDetails.java
+++ app/src/main/java/com/example/planner/venueDetails.java
@@ -1,5 +1,6 @@
 package com.example.planner;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,23 +10,55 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
 public class venueDetails extends AppCompatActivity {
 
     TextView client_name, event_type, subscription_plan, start_info, end_info, venue_name, billing_info, event_strength, location_info;
     ImageButton previous_button;
+    FirebaseFirestore fireStore;
+    FirebaseAuth fireAuth;
+    String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_venue_details);
-
-        //firebase fetching code
-
-        //setting all texts
-
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
+        //firebase fetching code
+
+        //setting all texts
+        DocumentReference dockRefre = fireStore.collection("customer").document(userId);
+        dockRefre.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                client_name.setText(value.getString("Full Name"));
+            }
+        });
+        DocumentReference dockRefre1 = fireStore.collection("eventChoose").document(userId);
+        dockRefre1.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
+                event_type.setText(value.getString("Event Name"));
+                subscription_plan.setText(value.getString("Plan Choosen"));
+                start_info.setText(value.getString("startDate"));
+                end_info.setText(value.getString("endDate"));
+                venue_name.setText(value.getString("venueName"));
+                billing_info.setText(value.getString("Completed"));
+                event_strength.setText(value.getString("numberOfpeople"));
+                location_info.setText(value.getString("location"));
+
+
+            }
+        });
+
 
         find_View_By_Id();
         billing_info.setOnClickListener(new View.OnClickListener() {
@@ -45,6 +78,9 @@ public class venueDetails extends AppCompatActivity {
     }
 
     private void find_View_By_Id() {
+        fireAuth = FirebaseAuth.getInstance();
+        fireStore = FirebaseFirestore.getInstance();
+        userId = fireAuth.getCurrentUser().getUid();
         client_name = findViewById(R.id.client_name);
         previous_button = findViewById(R.id.previous_details_button);
         event_type = findViewById(R.id.type_of_event);
