60ba8dbb25b0c38cbaddef9201ac940921701315
Arjun S Pramod <ascorncup@gmail.com>
2022-01-23 14:04:06 +0530

    custom dialog updated

---
+++ app/src/main/java/com/example/planner/foodMenu.java
@@ -0,0 +1,14 @@
+package com.example.planner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class foodMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_menu);
+    }
+}
